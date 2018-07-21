635f5515168fca117f155d864b0bc64a595ad3e0
Kevin Ngo <me@ngokevin.com>
2018-07-21 23:00:19 +0200

    difficulty menu highlight active

--- src/components/menu-difficulty-select.js
+++ src/components/menu-difficulty-select.js
@@ -3,6 +3,35 @@ var bindEvent = require('aframe-event-decorators').bindEvent;
 /**
  * Select difficulty.
  */
+AFRAME.registerComponent('menu-difficulty-option', {
+  dependencies: ['material'],
+
+  schema: {
+    activeDifficulty: {default: ''}
+  },
+
+  init: function () {
+    this.defaultColor = this.el.getAttribute('material').color;
+  },
+
+  update: function () {
+    var el = this.el;
+    let optionValue = this.el.closest('[data-bind-for-value]').dataset.bindForValue;
+    if (optionValue === this.data.activeDifficulty) {
+      el.setAttribute('animation__mouseenter', 'enabled', false);
+      el.setAttribute('animation__mouseleave', 'enabled', false);
+      el.setAttribute('material', 'color', '#4BF');
+    } else {
+      el.setAttribute('material', 'color', this.defaultColor);
+      el.setAttribute('animation__mouseenter', 'enabled', true);
+      el.setAttribute('animation__mouseleave', 'enabled', true);
+    }
+  },
+});
+
+/**
+ * Select difficulty.
+ */
 AFRAME.registerComponent('menu-difficulty-select', {
   init: function () {
     this.el.sceneEl.addEventListener('menuchallengeselect', () => {
@@ -13,8 +42,9 @@ AFRAME.registerComponent('menu-difficulty-select', {
   },
 
   click: bindEvent(function (evt) {
-    this.el.emit('menudifficultyselect',
-                 evt.target.closest('.difficultyOption').dataset.difficulty,
-                 false);
+    this.el.sceneEl.emit(
+      'menudifficultyselect',
+      evt.target.closest('.difficultyOption').dataset.difficulty,
+      false);
   })
 });
