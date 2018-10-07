cfe5a3fc2080c85c2f1217f12b1aa39f5b3dd34c
Kevin Ngo <me@ngokevin.com>
2018-10-06 21:36:15 -0700

    fix particle player error

--- src/components/particleplayer.js
+++ src/components/particleplayer.js
@@ -308,6 +308,8 @@ AFRAME.registerComponent('particleplayer', {
       }
     }
 
+    if (found === -1) { return; }
+
     particleSystem = this.particleSystems[found];
     particleSystem.active = true;
     particleSystem.loopCount = 1;
