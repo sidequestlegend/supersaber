b70ae20b1087b03bf83bc58f48bb9a263cd0e723
Kevin Ngo <me@ngokevin.com>
2018-10-15 17:18:39 -0700

    score text fade in

---
+++ src/components/score-texts.js
@@ -0,0 +1,40 @@
+/**
+ * Score text fade in animation.
+ */
+AFRAME.registerComponent('score-texts', {
+  dependencies: {
+    isSongLoading: {default: false}
+  },
+
+  init: function () {
+    this.textEls = this.el.querySelectorAll('[text]');
+
+    for (let i = 0; i < this.textEls.length; i++) {
+      this.textEls[i].setAttribute('animation__fadein', {
+        autoplay: false,
+        property:'components.text.material.uniforms.opacity.value',
+        delay: 250,
+        dur: 750,
+        easing: 'easeInOutCubic',
+        from: 0,
+        to: 1
+      });
+    }
+  },
+
+  update: function (oldData) {
+    // Finished loading.
+    if (oldData.isSongLoading && !this.data.isSongLoading) {
+      for (let i = 0; i < this.textEls.length; i++) {
+        this.textEls[i].components['animation__fadein'].beginAnimation();
+      }
+    }
+
+    // Started loading.
+    if (!oldData.isSongLoading && this.data.isSongLoading) {
+      for (let i = 0; i < this.textEls.length; i++) {
+        this.textEls[i].components.text.material.uniforms.opacity.value = 0;
+      }
+    }
+  }
+});
