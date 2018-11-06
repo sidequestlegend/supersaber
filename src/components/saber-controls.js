2131434ba0c4785301f427758ba711a347319dd8
Diego Marcos <diego.marcos@gmail.com>
2018-11-05 21:20:39 -0800

    Increase stroke min speed

--- src/components/saber-controls.js
+++ src/components/saber-controls.js
@@ -6,7 +6,7 @@ AFRAME.registerComponent('saber-controls', {
     bladeEnabled: {default: false},
     hand: {default: 'right', oneOf: ['left', 'right']},
     isPaused: {default: false},
-    strokeMinSpeed: {default: 5000},
+    strokeMinSpeed: {default: 10000},
     strokeMinAngle: {default: 5}
   },
 
