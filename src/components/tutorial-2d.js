5f580a41c059fa337aee8b6cb463d7519add9c15
Kevin Ngo <me@ngokevin.com>
2018-11-17 01:54:57 -0800

    hide loading canvas during load

--- src/components/tutorial-2d.js
+++ src/components/tutorial-2d.js
@@ -1,5 +1,10 @@
 AFRAME.registerComponent('tutorial-2d', {
+  init: function () {
+    this.el.sceneEl.canvas.style.display = 'none';
+  },
+
   play: function () {
+    this.el.sceneEl.canvas.style.display = 'block';
     document.getElementById('tutorial2d').style.display = 'none';
   }
 });
